477f857f761d143c7af3e377eb7664d91a436b76
ystk0327 <81604288+ystk0327@users.noreply.github.com>
2021-05-16 11:09:05 +0900

    テスト

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import "./styles.css";
+var var1 = 100;
+console.log(var1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+let var2 = 200;
+console.log(var2);
+
+var2 = 300;
+console.log(var2);
+
+console.log("こんにちは");
